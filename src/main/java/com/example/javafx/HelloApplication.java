c8d50bbf7bf7bd1a4187dad381b118581d00612a
Ander <amadinabeitia@goierrieskola.org>
2024-11-21 16:21:21 +0100

    Lehen commit-a

---
+++ src/main/java/com/example/javafx/HelloApplication.java
@@ -0,0 +1,30 @@
+package com.example.javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        //Opción 1: Crear todas la etiquetas desde JAVA
+        Label label = new Label("Hello World");
+
+
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
